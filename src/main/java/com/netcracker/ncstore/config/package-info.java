89fda30fc3fcd8d947153251e59a1cf11730181d
Mikhail Linker <work.michaellinker@gmail.com>
2021-10-19 13:15:55 +0300

    NC-9 Empty package-infos was added

---
+++ src/main/java/com/netcracker/ncstore/config/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains all configuration classes
+ * with the @Configuration annotation
+ */
+package com.netcracker.ncstore.config;
---
+++ src/main/java/com/netcracker/ncstore/web/package-info.java
@@ -0,0 +1,1 @@
+package com.netcracker.ncstore.web;

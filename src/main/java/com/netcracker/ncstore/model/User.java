cde8a6e3f407f7c7c26192268242acd53b9af591
Afomin <284718@niuitmo.ru>
2021-10-27 18:28:36 +0300

    Disclosed imports

--- src/main/java/com/netcracker/ncstore/model/User.java
+++ src/main/java/com/netcracker/ncstore/model/User.java
@@ -1,8 +1,17 @@
 package com.netcracker.ncstore.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import java.util.List;
 
 

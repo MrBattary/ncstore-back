bb1a324002d7c870770c01ce3af78d9788a1876c
Afomin <284718@niuitmo.ru>
2021-10-28 01:52:00 +0300

    NS-44 Added basic query methods

--- src/main/java/com/netcracker/ncstore/repository/UserRepository.java
+++ src/main/java/com/netcracker/ncstore/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.netcracker.ncstore.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+    boolean existsByEmail(String email);
 }

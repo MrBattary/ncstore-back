526375614f057b3365e009889294eecb1ce946a6
Mikhail Linker <work.michaellinker@gmail.com>
2021-11-11 20:22:22 +0300

    NS-142 JPA Query standard

--- src/main/java/com/netcracker/ncstore/repository/PersonRepository.java
+++ src/main/java/com/netcracker/ncstore/repository/PersonRepository.java
@@ -3,11 +3,10 @@ package com.netcracker.ncstore.repository;
 import com.netcracker.ncstore.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface PersonRepository extends JpaRepository<Person, UUID> {
-    @Query("SELECT p FROM Person p WHERE p.user.email = :email")
-    Person findPersonByUserEmail(@Param("email") String email);
+    @Query("SELECT p FROM Person p WHERE p.user.email = ?1")
+    Person findPersonByUserEmail(String email);
 }
